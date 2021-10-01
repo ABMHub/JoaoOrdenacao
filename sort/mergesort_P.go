048142eda586731f1bb1aef028710e32c517670c
aledx correa <totalxpt@hotmail.com>
2021-10-01 18:08:05 -0300

    sem runtime e comentado

--- sort/mergesort_P.go
+++ sort/mergesort_P.go
@@ -7,47 +7,43 @@ import(
 func Mergesort_P(arr []util.T, cmp func(util.T, util.T) bool){
 	
 	p_threads := util.GetThreadLimit() + 1
-	var fin, part, npart, arrSize int
-	bF := make([]util.Pair, p_threads + 1)
-	bolH := make([]bool, p_threads + 1)
+	var fin, part, npart, arrSize int // ultimo idx do arr, particoes, arruma as particoes, ...
+	bF := make([]util.Pair, p_threads + 1) // guarda begin e end de cada particao.
+	bolH := make([]bool, p_threads + 1) // fala se a particao esta ou nao preparada para modificacao
 	arrSize = len(arr)
 	fin = arrSize - 1
-	part, npart = (fin + 1)/p_threads, (fin+1)%p_threads
-
-	if(npart==fin){
-		npart--
-	}
-
-	bF[0] = util.Pair{0,part+npart-1}
+	part, npart = arrSize/p_threads, arrSize%p_threads // pega as particoes
+	bF[0] = util.Pair{0,part+npart-1} // inicializa a primeira particao
 
 	for j := 1;; j++{
 		bolH[j-1]=false
 
 		go func(idx int){
 			defer func(){
-				bolH[idx]=true
+				bolH[idx]=true // no fim da thread, fala q a particao ta preparada.
 			}()
 			Mergesort(arr, bF[idx].Fst.(int), bF[idx].Snd.(int), cmp)
 		}(j-1)
-		if(bF[j-1].Snd.(int) == fin){
+		if(bF[j-1].Snd.(int) == fin){ // checagem de ultima particao.
 			break
 		}
 		bF[j] = util.Pair{bF[j-1].Snd.(int) + 1, bF[j-1].Snd.(int) + part}
+		// calculo para proxima particao.
 	}
 
-	for bF[0].Snd.(int) - bF[0].Fst.(int) + 1 < len(arr){
-		for i := 0; i < p_threads; i++{
-			if bolH[i]{
-				for j := i + 1; j < p_threads; j++{
-					if(bF[i].Snd.(int) == bF[j].Fst.(int)-1 && bolH[j]){
+	for bF[0].Snd.(int) - bF[0].Fst.(int) + 1 < arrSize{ 
+		for i := 0; i < p_threads; i++{ // checa todas as particoes.
+			if bolH[i]{ // particao ta pronta
+				for j := i + 1; j < p_threads; j++{ // checa pela proxima particao pronta
+					if(bF[i].Snd.(int) == bF[j].Fst.(int)-1 && bolH[j]){ // particoes "tocando" + particao pronta.
 						bolH[i]=false
 						bolH[j]=false
 						go func(idxa, idxb int){
 							defer func(){
-								bF[idxa].Snd=bF[idxb].Snd
-								bolH[idxa]=true
+								bF[idxa].Snd=bF[idxb].Snd // particao A "engole" a particao B 
+								bolH[idxa]=true // so a primeira particao fica pronta.
 							}()
-							merge(arr, bF[idxa].Fst.(int), bF[idxa].Snd.(int), 
+							merge(arr, bF[idxa].Fst.(int), bF[idxa].Snd.(int),
 														bF[idxb].Snd.(int), cmp)
 						}(i,j)
 						break
