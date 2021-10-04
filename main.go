106c2fa4adc24ecbad9a461d3f53d861fde13494
unknown <totalxpt@hotmail.com>
2021-10-04 12:08:32 -0300

    gluglu

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	arr := make([]util.T,1e6)
 	for i := 0; i < 1e6; i++{
 		file.Read(temp)
-		arr[i] = int(binary.BigEndian.Uint32(temp))
+		arr[i] = int(binary.LittleEndian.Uint32(temp))
 	}
-	sort.Mergesort_P(arr, 0, len(arr)-1, func(a, b util.T)bool{
+	sort.Mergesort_P(arr,func(a, b util.T)bool{
 		return a.(int) > b.(int)
 	})
 	fmt.Println(arr)
--- sort/mergesort_P.go
+++ sort/mergesort_P.go
@@ -4,33 +4,32 @@ import(
 	"sortAlgorithms/util"
 )
 
-func Mergesort_P(arr []util.T,	 cmp func(util.T, util.T) bool){
+func Mergesort_P(arr []util.T, cmp func(util.T, util.T) bool){
 	
 	p_threads := util.GetThreadLimit() + 1
-	var part, npart int // ultimo idx do arr, particoes, arruma as particoes, ...
+	var begin, fin, part, npart int // ultimo idx do arr, particoes, arruma as particoes, ...
 	bF := make([]util.Pair, p_threads + 1) // guarda begin e end de cada particao.
 	bolH := make([]bool, p_threads + 1) // fala se a particao esta ou nao preparada para modificacao
-	
+	begin = 0
+	fin = len(arr)-1	
 	part, npart = (fin-begin+1)/p_threads, (fin-begin+1)%p_threads // pega as particoes
 	
 	bF[0] = util.Pair{0,part+npart-1} // inicializa a primeira particao
-
-	for j := 1;; j++{
-		bolH[j-1]=false
+	for j := 0; j < p_threads-1; j++{
+		bolH[j]=false
 
 		go func(idx int){
 			defer func(){
 				bolH[idx]=true // no fim da thread, fala q a particao ta preparada.
 			}()
 			Mergesort(arr, bF[idx].Fst.(int), bF[idx].Snd.(int), cmp)
-		}(j-1)
-		if(bF[j-1].Snd.(int) == fin){ // checagem de ultima particao.
-			break
-		}
-		bF[j] = util.Pair{bF[j-1].Snd.(int) + 1, bF[j-1].Snd.(int) + part}
+		}(j)
+		bF[j+1] = util.Pair{bF[j].Snd.(int) + 1, bF[j].Snd.(int) + part}
 		// calculo para proxima particao.
 	}
-
+	bolH[p_threads-1]=true
+	Mergesort(arr, bF[p_threads-1].Fst.(int), bF[p_threads-1].Snd.(int), cmp)
+		
 	for bF[0].Snd.(int) - bF[0].Fst.(int) + 1 < fin-begin+1{ 
 		for i := 0; i < p_threads; i++{ // checa todas as particoes.
 			if bolH[i]{ // particao ta pronta
