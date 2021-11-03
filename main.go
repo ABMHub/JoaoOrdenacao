9dcdeaccf2e1b0b54ef50815f581552da66f6796
DYosplay <joaopedrofelix2013@hotmail.com>
2021-11-03 02:35:44 -0300

    pronto para apresentar

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func main() {
 
 	// Chama a ordenacao
 	sort.Merge_Files("Integerscpp2.bin", "quick-sort", elem_size, batch_size, number_of_processors, util.ReadIntegers, util.CompareInt, util.FragmentBin, util.WriteIntegers)
-	
+
 	// Primeiros 100 elementos do arquivo a ser ordenado
 	fmt.Print("\n\n100 primeiros inteiros a serem ordenados:\n\n")
 	file1, err1 := os.Open("Integerscpp2.bin") // abre arquivo
-	if err1 != nil {                     // se der erro cancela tudo
+	if err1 != nil {                           // se der erro cancela tudo
 		log.Fatal("Erro na leitura do arquivo binario com os inteiros a serem ordenados", err1) //
 		defer file1.Close()                                                                     //
 	}
@@ -31,20 +31,20 @@ func main() {
 	// Mostra os 100 primeiros elementos depois de o arquivo ter sido ordenado
 	fmt.Print("\n\n100 primeiros inteiros depois de ordenados:\n\n")
 	file1, err1 = os.Open("Sorted.bin") // abre arquivo
-	if err1 != nil {                     // se der erro cancela tudo
+	if err1 != nil {                    // se der erro cancela tudo
 		log.Fatal("Erro na leitura do arquivo binario com os inteiros ordenados", err1) //
-		defer file1.Close()                                                                     //
+		defer file1.Close()                                                             //
 	}
 	fmt.Println(util.ReadIntegers(file1, 100))
-	
+
 	/* ###################################################################################### */
 
 	// Json
-	
+
 	// file1, err1 := os.Open("dump.json")
 	// if err1 != nil {
 	// 	log.Fatal("Erro na leitura do arquivo binario com os inteiros a serem ordenados", err1)
-	//  	defer file1.Close()    
+	//  	defer file1.Close()
 	// }
 	// sort.SortJson(file1, util.CompareLikes)
 }
--- sort/decode_json.go
+++ sort/decode_json.go
@@ -1,21 +1,22 @@
 package sort
 
 import (
-    "encoding/json"
-    "os"
-    "io/ioutil"
-    "sortAlgorithms/util"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"sortAlgorithms/util"
 )
 
-func SortJson(file *os.File,cmp util.Compare) {
-    var arr []util.Reddit
-    jsonData, _ := ioutil.ReadAll(file)
-    json.Unmarshal(jsonData, &arr)    
-    slice := make([]util.T, len(arr))
-    for i := range arr {
-        slice[i] = arr[i]
-    }
-    Mergesort_P(slice, cmp)
-    res,_ := json.MarshalIndent(slice, "", " ")
-    ioutil.WriteFile("Sorted.json", res, os.ModePerm)    
+func SortJson(file *os.File, cmp util.Compare) {
+	var arr []util.Reddit
+	jsonData, _ := ioutil.ReadAll(file)
+	json.Unmarshal(jsonData, &arr)
+	slice := make([]util.T, len(arr))
+	for i := range arr {
+		slice[i] = arr[i]
+	}
+	util.SetThreadLimit(10)
+	Mergesort_P(slice, cmp)
+	res, _ := json.MarshalIndent(slice, "", " ")
+	ioutil.WriteFile("Sorted.json", res, os.ModePerm)
 }
